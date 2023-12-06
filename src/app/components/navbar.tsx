964596ee0efc24b29b4c36e97df412064fc4b573
tanometro <angelogrillo@bitecnologias.com>
2023-12-07 00:36:27 +0100

    Enrutado de parametros

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -6,9 +6,9 @@ const NavBar: React.FC = () => {
     <div className="flex justify-between items-center p-4">
       <div className="flex items-center">
         <img src="https://i.ibb.co/BrF41j4/zeus-1.png" alt="" className="w-10 h-10 p-1 bg-white rounded-lg ml-2" />
-        <p className="text-white ml-2 hidden lg:inline font-serif text-2xl ml-4">Olimpus</p>
+        <p className="text-white hidden lg:inline font-serif text-2xl ml-4">Olimpus</p>
       </div>
-      <input type="text" className="block sm:w-60 h-7 md:w-80 lg:w-3/5 h-10 p-3 pl-4 rounded-lg bg-gray-300 focus:outline-none" />
+      <input type="text" className="block sm:w-60 md:w-80 lg:w-3/5 h-10 p-3 pl-4 rounded-lg bg-gray-300 focus:outline-none" />
       <div className="flex">
         <img src="https://i.ibb.co/X22nzYG/user.png" alt="" className="w-10 h-10 p-1 bg-white rounded-lg mr-4" />
         <img src="https://i.ibb.co/h8rNp7y/shopping.png" alt="" className="w-10 h-10 p-1 bg-white rounded-lg mr-2" />
