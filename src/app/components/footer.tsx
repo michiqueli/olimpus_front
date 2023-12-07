8d4ff2f0c5446f2d83e6c15bb05c24e19ccefa79
Mica Liste <micaelaliste@gmail.com>
2023-12-07 20:38:52 -0300

    footer, falta logo

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,97 +1,58 @@
 "use client"
+// import zeus from "../../assets/zeus.png"
 
 const Footer: React.FC=()=>{
     return(
-        <footer className="bg-white dark:bg-gray-900">
-            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-                <div className="md:flex md:justify-between">
-                <div className="mb-6 md:mb-0">
-                    {/* <a href="https://flowbite.com/" className="flex items-center"> */}
-
-                    //FALTA CAMBIAR SRC
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="Olimpus Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Olimpus</span>
-                    {/* </a> */}
-                </div>
-                <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                    <div>
-                        {/* <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2> */}
-                        <ul className="text-gray-500 dark:text-gray-400 font-medium">
-
-                            //FALTA CAMBIAR LOS HREF
-                            <li className="mb-4">
-                                <a href="https://flowbite.com/" className="hover:underline">About</a>
-                            </li>
-                            <li>
-                                <a href="https://tailwindcss.com/" className="hover:underline">Franquicias</a>
-                            </li>
-                            <li>
-                                <a href="https://tailwindcss.com/" className="hover:underline">Locales</a>
-                            </li>
-                        </ul>
+        <footer className="bg-yellow-300 relative">
+            <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
+                <nav className="flex flex-wrap justify-center -mx-5 -my-2">
+                    <div className="px-5 py-2">
+                        <a href="#" className="text-base leading-6 text-black-500 hover:text-blue-800">
+                            About
+                        </a>
                     </div>
-                    <div>
-                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                        <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                            <li className="mb-4">
-                                <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
-                            </li>
-                            <li>
-                                <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
-                            </li>
-                        </ul>
+                    <div className="px-5 py-2">
+                        <a href="#" className="text-base leading-6 text-black-500 hover:text-blue-800">
+                            Stores
+                        </a>
                     </div>
-                    {/* <div>
-                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                        <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                            <li className="mb-4">
-                                <a href="#" className="hover:underline">Privacy Policy</a>
-                            </li>
-                            <li>
-                                <a href="#" className="hover:underline">Terms &amp; Conditions</a>
-                            </li>
-                        </ul>
-                    </div> */}
+                    <div className="px-5 py-2">
+                        <a href="#" className="text-base leading-6 text-black-500 hover:text-blue-800">
+                            Contact
+                        </a>
+                    </div>
+                    <div className="px-5 py-2">
+                        <a href="#" className="text-base leading-6 text-black-500 hover:text-blue-800">
+                            Team
+                        </a>
+                    </div>
+                
+                </nav>
+                <div className="flex justify-center mt-8 space-x-6">
+                    <a href="#" className="text-black-400 hover:text-blue-800">
+                        <span className="sr-only">Gmail</span>
+                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+                            <path fillRule="evenodd" d="M21.35 3H2.65C1.87 3 1.25 3.62 1.25 4.38v15.24c0 .76.62 1.38 1.38 1.38h18.7c.76 0 1.38-.62 1.38-1.38V4.38c0-.76-.62-1.38-1.38-1.38zm-2.18 2.71l-7.07 5.67-7.07-5.67H19.17zM2.65 19.5V6.62l7.03 5.62L2.65 19.5zm2.18-1.09l2.89-2.32 1.47 1.18L6.67 18H4.83zM2.65 5.12V4h18.7v1.12l-9.35 7.48-9.35-7.48z" clipRule="evenodd"></path>
+                        </svg>
+                    </a>
+                    <a href="#" className="text-black-400 hover:text-blue-800">
+                        <span className="sr-only">WhatsApp</span>
+                        <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+                            <path fillRule="evenodd"  d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z"  clipRule="evenodd"></path>
+                        </svg>
+                    </a>
+                    <a href="#" className="text-black-400 hover:text-blue-800">
+                        <span className="sr-only">GitHub</span>
+                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+                            <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd"></path>
+                        </svg>
+                    </a>
                 </div>
-            </div>
-            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <div className="sm:flex sm:items-center sm:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Olimpus™</a>.
-                </span>
-                <div className="flex mt-4 sm:justify-center sm:mt-0">
-                    // FALTA AGREGAR ENLACES QUE LLEVEN  A ....
-                    <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
-                                <path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
-                            </svg>
-                        <span className="sr-only">Whatsapp page</span>
-                    </a>
-                    <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16">
-                                <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z"/>
-                            </svg>
-                        <span className="sr-only">Email page</span>
-                    </a>
-                    <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
-                            <path fillRule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clip-rule="evenodd"/>
-                        </svg>
-                        <span className="sr-only">LinkedIn page</span>
-                    </a>
-                    <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path fillRule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
-                        </svg>
-                        <span className="sr-only">GitHub account</span>
-                    </a>
-                    {/* <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z" clip-rule="evenodd"/>
-                        </svg>
-                        <span className="sr-only">Dribbble account</span>
-                    </a> */}
-                </div>
-            </div>
+                <p className="mt-8 text-base leading-6 text-center text-black-400">
+                    © 2023 Olimpus Company.
+                </p>
+
+                {/* <img src="zeus.png"></img>  */}
             </div>
         </footer>
     )    
