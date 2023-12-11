479226b8499c551402e9348993e1be3dd6f7bd28
SGuevaraOlie <santiguevara98@gmail.com>
2023-12-10 22:44:04 -0300

    cambios en la navegacion de navbar e implementación de validator para login

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useRouter } from "next/navigation";
-import { signIn } from "next-auth/react";
 
 const NavBar: React.FC = () => {
   const router = useRouter();
@@ -8,15 +7,19 @@ const NavBar: React.FC = () => {
   <div className="bg-black">
     <div className="flex justify-between items-center p-4">
       <div className="flex items-center">
-        <img src="https://i.ibb.co/BrF41j4/zeus-1.png" alt="" className="w-10 h-10 p-1 bg-white rounded-lg ml-2" />
+        <button onClick={() => router.push('/')}>
+          <img src="https://i.ibb.co/BrF41j4/zeus-1.png" alt="" className="w-10 h-10 p-1 bg-white rounded-lg ml-2" />
+        </button>
         <p className="text-white hidden lg:inline font-serif text-2xl ml-4">Olimpus</p>
       </div>
       <input type="text" className="block sm:w-60 md:w-80 lg:w-3/5 h-10 p-3 pl-4 rounded-lg bg-gray-300 focus:outline-none" />
       <div className="flex">
-        <button onClick={() => signIn()}>
+        <button onClick={() => router.push('/login')}>
           <img src="https://i.ibb.co/X22nzYG/user.png" alt="" className="w-10 h-10 p-1 bg-white rounded-lg mr-4" />
         </button>
-        <img src="https://i.ibb.co/h8rNp7y/shopping.png" alt="" className="w-10 h-10 p-1 bg-white rounded-lg mr-2" />
+        <button onClick={() => router.push('/cart')}>
+          <img src="https://i.ibb.co/h8rNp7y/shopping.png" alt="" className="w-10 h-10 p-1 bg-white rounded-lg mr-2" />
+        </button>
       </div>
     </div>
   </div>
