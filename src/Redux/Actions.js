9eb8a11b2b2c8f89322265c9e0407be0cf7ae9be
michqiueli <michiqueli@gmail.com>
2023-12-12 15:43:39 -0300

    arreglo de errores

--- src/Redux/Actions.js
+++ src/Redux/Actions.js
@@ -1,6 +1,5 @@
 
 import axios from "axios"
-import { setSearchedProducts } from "./sliceProducts";
 import { setUsers } from "./sliceUsers";
 import { setSearchedProducts, setProductTypes } from "./sliceProducts";
 
