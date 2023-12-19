666cf1d417d6ee3884f03b017cf240992bf40f8f
SGuevaraOlie <santiguevara98@gmail.com>
2023-12-18 22:28:59 -0300

    cambios en navbar: logo e icons, cardsSlider ajustes esteticos

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -5,21 +5,20 @@ import SearchBar from "./searchbar";
 const NavBar: React.FC = () => {
   const router = useRouter();
   return (
-  <div className="bg-yellow-300 w-full flex justify-center items-center">
-    <div className="flex justify-between items-center p-4 w-[80%]">
+  <div className="bg-yellow-300 w-full h-28 flex justify-center items-center">
+    <div className="flex justify-between items-center p-4 w-[95%]">
       <div className="flex items-center">
         <button onClick={() => router.push('/')}>
           <img src="/zeus.png" alt="" className="w-24 h-24 hover:scale-110" />
-          <p className="text-white hidden lg:inline font-serif text-2xl">Olimpus</p>
         </button>
       </div>
       <SearchBar />
       <div className="flex">
         <button onClick={() => router.push('/login')}>
-          <img src="/user.png" alt="" className="w-16 h-16 mr-6 hover:scale-110" />
+          <img src="/user.png" alt="" className="w-11 h-11 mr-6 hover:scale-110" />
         </button>
         <button onClick={() => router.push('/cart')}>
-          <img src="/shopping.png" alt="" className="w-16 h-16 hover:scale-110" />
+          <img src="/shopping.png" alt="" className="w-11 h-11 hover:scale-110" />
         </button>
       </div>
     </div>
