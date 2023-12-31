21d1eb3794e0fefa5f724fcd20e07216adbcbf7f
SGuevaraOlie <santiguevara98@gmail.com>
2023-12-31 16:52:06 -0300

    register minimos cambios para merge

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -41,28 +41,30 @@ const loginPage = () => {
     return (
         <div className='flex flex-col justify-center items-center my-4'>
             <img src="/olimpus.png" alt="" className='h-48 my-4'/>
-            <form onSubmit={handleSubmit} className='flex flex-col items-center h-full w-3/12 mb-4 bg-gray-100 mx-2 border border-gray-300 rounded-lg shadow'>
-                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 className='text-xl font-bold leading-tight tracking-tight md:text-2xl dark:text-white'>Inicia Sesión en Olimpus Shop</h1>
-                </div>
-                <div className='flex-col justify-start w-10/12'>
-                    <label className="block mb-2 ml-2 text-sm font-medium dark:text-white">Tu email:</label>
-                    <Field placeholder='Tu email' name='email' onChange={handleChange} value={credentials.email}/>
-                </div>
-                {error.email && <p className="text-red-500">{error.email}</p>}
-                <div className='flex-col justify-start w-10/12'>
-                    <label className="block mb-2 ml-2 text-sm font-medium dark:text-white">Tu contraseña:</label>
-                    <Field placeholder='Tu contraseña' name='password' onChange={handleChange} value={credentials.password}/>
-                </div>
-                {/* {error.password && <p className="text-red-500">{error.password}</p>} */}
+            <div className='flex flex-col items-center h-full w-3/12 mb-4 bg-gray-100 mx-2 border border-gray-300 rounded-lg shadow'>
+                <form onSubmit={handleSubmit} className='flex flex-col items-center h-full w-full mx-2'>
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className='text-xl font-bold leading-tight tracking-tight md:text-2xl dark:text-white'>Inicia Sesión en Olimpus Shop</h1>
+                    </div>
+                    <div className='flex-col justify-start w-10/12'>
+                        <label className="block mb-2 ml-2 text-sm font-medium dark:text-white">Tu email:</label>
+                        <Field placeholder='Tu email' name='email' onChange={handleChange} value={credentials.email}/>
+                    </div>
+                    {error.email && <p className="text-red-500">{error.email}</p>}
+                    <div className='flex-col justify-start w-10/12'>
+                        <label className="block mb-2 ml-2 text-sm font-medium dark:text-white">Tu contraseña:</label>
+                        <Field placeholder='Tu contraseña' name='password' onChange={handleChange} value={credentials.password}/>
+                    </div>
+                    {/* {error.password && <p className="text-red-500">{error.password}</p>} */}
 
-                <button className='my-4 w-10/12 text-xl bg-yellow-200 hover:bg-yellow-300 text-black font-normal py-2 px-4 rounded-full'>Inicia Sesión</button>
+                    <button className='my-4 w-10/12 text-xl bg-yellow-200 hover:bg-yellow-300 text-black font-normal py-2 px-4 rounded-full'>Inicia Sesión</button>
+                </form>
                 <button className='mb-4 w-10/12 text-xl bg-yellow-200 hover:bg-yellow-300 text-black font-normal py-2 px-4 rounded-full' onClick={() => signIn()}>Iniciar Sesión con Google</button>
                 <div className='flex flex-row'>
                     <h1 className='block mb-2 ml-2 text-sm font-medium dark:text-white'>¿No tienes cuenta?</h1>
                     <button onClick={() => router.push('/register')} className="block mb-2 ml-2 text-sm font-medium dark:text-white hover:text-blue-800">Registrate.</button>
                 </div>
-            </form>
+            </div>
         </div>
     )
 }
