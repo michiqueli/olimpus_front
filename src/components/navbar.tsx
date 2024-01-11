95432a5f0c2018f8ecfdc60c81287c1332e30161
Nicolas Mennichelli <74836386+michiqueli@users.noreply.github.com>
2024-01-11 10:57:32 -0300

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -54,8 +54,20 @@ const NavBar: React.FC = () => {
                 onClick={() => setDropdownVisible(!dropdownVisible)}
                 className="flex items-center focus:outline-none"
               >
-                <img src="/user.png" alt="" className="w-11 h-11 mr-6 hover:scale-110" />
-              </button>
+                {session?.user ? (
+                    <img
+                      src={session?.user.image ?? "No IMG"}
+                      alt="No IMG"
+                      className="w--11 h-11 mr-6 hover:scale-110"
+                    />
+                  ) : (
+                    <img
+                      src="/user.png"
+                      alt=""
+                      className="w-11 h-11 mr-6 hover:scale-110"
+                    />
+                  )}
+                </button>
               {dropdownVisible && (
                 <div className="w-36 absolute top-12 right-0 bg-white border border-gray-300 p-3 shadow-md rounded-md z-20">
                 {
