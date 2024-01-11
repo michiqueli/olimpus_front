71086637e63631469b46cb9bb40f116a91ab8058
Mica Liste <micaelaliste@gmail.com>
2024-01-11 20:41:09 -0300

    carrito funcionando

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -5,48 +5,55 @@ import { useProduct } from "@/context/CartContext";
 const Cart: React.FC<CartProps> = ({ isOpen, setIsOpen, onClose }) =>{
     const router= useRouter()
     const {contextProducts, total, totalProducts, deleteAllProducts, deleteProduct} = useProduct();
+    const renderedProductIds = new Set();
 
     return(
         <>
         {isOpen && (
-            <div className="relative z-10" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-                <div className="fixed inset-0 overflow-hidden">
-                <div className="absolute inset-0 overflow-hidden">
-                    <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-                    <div className="pointer-events-auto w-screen max-w-md">
-                        <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-                        <div className="flex-1 px-4 py-6 sm:px-6">
-                            <div className="flex items-start justify-between">
-                            <h2 className="text-lg font-medium text-gray-900" id="slide-over-title">Carrito de compras</h2>
-                                <div className="ml-3 flex h-7 items-center">
-                                <button onClick={onClose} type="button" className="relative -m-2 p-2 text-gray-400 hover:text-gray-500">X</button>
-                                </div>
-                            </div>
-                            
+        <div className="relative z-10" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <div className="pointer-events-auto w-screen max-w-md">
+                  <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                    <div className="flex-1 px-4 py-6 sm:px-6">
+                      <div className="flex items-start justify-between">
+                        <h2 className="text-lg font-medium text-gray-900" id="slide-over-title">
+                          Carrito de compras
+                        </h2>
+                        <div className="ml-3 flex h-7 items-center">
+                          <button onClick={onClose} type="button" className="relative -m-2 p-2 text-gray-400 hover:text-gray-500">
+                            X
+                          </button>
                         </div>
-                            <div>
-                            <p className="mt-0.5 text-sm text-gray-500 ml-20">Los productos en SALE no tienen cambio</p>
-                            </div>
-                            <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-                                {
-                                    contextProducts.length > 1 ? (
-
-                                        contextProducts.map(product => (
-                                            <div>
-                                                <h1>{product.name}</h1>
-                                                <h1>{product.price}</h1>
-                                                <h1>{product.description}</h1>
-                                            </div>
-                                        ))
-                                    )
-                                    :
-                                    <h1 className="text.black">No tenes agregado ningún producto al carrito wey</h1>
+                      </div>
+                    </div>
+                    <div>
+                      <p className="mt-0.5 text-sm text-gray-500 ml-20">Los productos en SALE no tienen cambio</p>
+                    </div>
+                    <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+                        {contextProducts.length > 0 ? (
+                            contextProducts.map((product) => {
+                            // Verificar si el producto ya se ha renderizado
+                                if (renderedProductIds.has(product.id)) {
+                                    return null; // Evitar volver a renderizar el mismo producto
                                 }
-          
-    
-
-
+                            // Agregar el ID del producto al conjunto
+                                renderedProductIds.add(product.id);
+                                return (
+                                    <div key={product.id}>
+                                    <img src={product.image} alt="image product" width={100} height={100} className="object-cover" />
+                                    <h1 onClick={() => router.push("/productDetail")}>{product.name}</h1>
+                                    <h1>{product.price}</h1>
+                                    <h1>{product.description}</h1>
+                                    </div>
+                                );
+                            })
+                            ) : (
+                                <h1 className="text.black">No tienes agregado ningún producto al carrito, wey</h1>
+                            )}
+                            <h1>cantidad: {totalProducts}</h1>
                             <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
                                 <button onClick={()=>router.push("")} className="flex items-center justify-center rounded-md border border-transparent bg-yellow-200 px-6 py-3 text-base font-medium text-black shadow-sm hover:bg-yellow-300 ">
                                 Iniciar compra
