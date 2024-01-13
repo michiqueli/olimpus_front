2119c590131f97706d2fcb73bdb867f057935519
MELSTFOR <mforgiarini.89@gmail.com>
2024-01-13 00:33:17 -0300

    integracion MP

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -15,6 +15,7 @@ const Cart: React.FC<CartProps> = ({ isOpen, setIsOpen, onClose }) => {
   const renderedProductIds = new Set();
 
   const checkout = async () => {
+    console.log(contextProducts);
     const data = {
       products: contextProducts.map((product) => ({
         title: product.name,
@@ -23,6 +24,8 @@ const Cart: React.FC<CartProps> = ({ isOpen, setIsOpen, onClose }) => {
         unit_price: product.price,
       })),
     };
+
+    console.log(data);
 
     try {
       const response = await axios.post(
